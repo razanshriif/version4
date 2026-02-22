0993c529ba4a5a7ad2a5ab561459bd0d3574ef6f
razanshriif <156240975+razanshriif@users.noreply.github.com>
2026-02-22 12:35:31 +0100

    fix: update server IP to 172.20.10.2

--- lumiere-new/src/environments/environment.prod.ts
+++ lumiere-new/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
 
   // 🌐 URL de PRODUCTION
   // Changez cette URL quand vous déployez sur un vrai serveur
-  apiUrl: 'http://192.168.217.88:8090/api',
+  apiUrl: 'http://172.20.10.2:8090/api',
 
   // OU si votre serveur de production utilise aussi le port 8090
   // apiUrl: 'http://votre-serveur.com:8090/api',
